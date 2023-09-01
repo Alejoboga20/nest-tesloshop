47a254e07a025acf9a416fa1da98700008e728cd
Alejo Boga <alejoboga20@gmail.com>
2023-09-01 12:14:02 -0300

    create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
