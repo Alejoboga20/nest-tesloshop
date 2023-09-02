9a24c2edfd5bb2caaba3bce0a9a66240d929f40a
Alejo Boga <alejoboga20@gmail.com>
2023-09-02 16:42:57 -0300

    update product

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,44 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @IsString()
+  @MinLength(3)
+  @IsOptional() // Make title optional for updates
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional() // Make price optional for updates
+  price?: number;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional() // Make stock optional for updates
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional() // Make sizes optional for updates
+  sizes?: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  @IsOptional() // Make gender optional for updates
+  gender?: string;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -65,8 +65,20 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product: ${updateProductDto}`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+    if (!product)
+      throw new BadRequestException(`Product with id: ${id} not found`);
+
+    try {
+      await this.productRepository.save(product);
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+    return product;
   }
 
   async remove(id: string) {
