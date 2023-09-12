13a144712f37f83a49a3713ad5bd28703a438591
Alejo Boga <alejoboga20@gmail.com>
2023-09-12 17:01:27 -0300

    verify role

--- src/auth/guards/user-role-guard/user-role-guard.guard.ts
+++ src/auth/guards/user-role-guard/user-role-guard.guard.ts
@@ -1,6 +1,13 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuardGuard implements CanActivate {
@@ -9,12 +16,21 @@ export class UserRoleGuardGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
     const validRoles: string[] = this.reflector.get(
       'roles',
       context.getHandler(),
     );
-    console.log({ validRoles });
 
-    return true;
+    if (!validRoles || validRoles.length === 0) return true;
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException('You do not have permission to access this');
   }
 }
