4c69ab98afc3459871019d0b6146966890f2e3b3
Alejo Boga <alejoboga20@gmail.com>
2023-09-12 16:49:32 -0300

    create guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
@@ -6,6 +13,7 @@ import { CreateUserDto, LoginUserDto } from './dto';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from 'src/common/decorators/raw-headers.decorator';
+import { UserRoleGuardGuard } from './guards/user-role-guard/user-role-guard.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,6 +29,7 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
+  /* Examples of custom decorators and guards */
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
@@ -29,5 +38,12 @@ export class AuthController {
     @RawHeaders('Authorization') headers: string[],
   ) {
     return { user, email, headers };
+  }
+
+  @Get('private-two')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuardGuard)
+  testingPrivateRouteTwo(@GetUser() user: User) {
+    return { user };
   }
 }
