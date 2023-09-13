1f3f2bee23d20576db8361a759d30527750ecc76
Alejo Boga <alejoboga20@gmail.com>
2023-09-13 11:56:59 -0300

    relate user to products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,10 +3,12 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
+import { User } from 'src/auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
@@ -38,6 +40,9 @@ export class Product {
 
   @Column('text', { array: true, default: [] })
   tags: string[];
+
+  @ManyToOne(() => User, (user) => user.product, { eager: true })
+  user: User;
 
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
