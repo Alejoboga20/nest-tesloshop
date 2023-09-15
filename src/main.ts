b19fe4a7ec9a114445fce9f461f3695fdf365cde
Alejo Boga <alejoboga20@gmail.com>
2023-09-15 12:33:28 -0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
@@ -13,6 +14,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Rest API')
+    .setDescription('Tesloshop API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
   logger.log(`App running on port: ${process.env.PORT || 3000}`);
