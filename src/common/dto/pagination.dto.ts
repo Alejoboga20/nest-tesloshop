31571360bd6c65976de7da27939ba8eeba7a02f9
Alejo Boga <alejoboga20@gmail.com>
2023-09-15 13:47:08 -0300

    document dtos

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,11 +1,22 @@
 import { IsOptional, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
+  @ApiProperty({
+    description: 'Limit of items per page',
+    example: 10,
+    required: false,
+  })
   @IsOptional()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    description: 'Offset of items per page',
+    example: 5,
+    required: false,
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number)
