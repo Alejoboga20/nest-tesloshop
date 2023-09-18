1cdccbfc77fa6a6fd1bc3b9696a277ae5e554a34
Alejo Boga <alejoboga20@gmail.com>
2023-09-18 15:33:25 -0300

    listen to events from client

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,11 +1,13 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessagesService } from './messages.service';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 enum Events {
   CLIENTS_UPDATED = 'clients-updated',
@@ -34,5 +36,10 @@ export class MessagesGateway
       Events.CLIENTS_UPDATED,
       this.messagesService.getConnectedClients(),
     );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log({ id: client.id, payload });
   }
 }
